4fc5ac970cb36941953be5bc732f82308f2112ae
edi-garsell46 <47446916+edi-garsell46@users.noreply.github.com>
2019-02-10 13:33:41 +0700

    Update __init__.py

--- xlotp/xlpy/__init__.py
+++ xlotp/xlpy/__init__.py
@@ -1,10 +1,10 @@
 from .config import Config
 from .client import XL
 
-__copyright__       = 'Copyright 2018 by Anggi Alberto'
+__copyright__       = 'Copyright 2018 By Edi'
 __version__         = '1.0.0'
-__author__          = 'Anggi Alberto'
-__author_email__    = 'albertoanggi@gmail.com'
-__url__             = 'http://github.com/albertoanggi/xl-py'
+__author__          = 'Edi Garsell'
+__author_email__    = 'edi.garsell@gmail.com'
+__url__             = 'https://github.com/edi-garsell46/semut.git'
 
 __all__ = ['Config','XL']
